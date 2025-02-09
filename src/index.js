9601204db318e3d351271ddb80f8285a7f842bdf
Mohd Ayaan <mohdayaan8071@gmail.com>
2025-02-09 13:29:53 +0530

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const {port} = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
