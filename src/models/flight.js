a3c64e386e0c5e38af393edcc99e90455da26e4f
Mohd Ayaan <mohdayaan8071@gmail.com>
2025-02-18 01:18:38 +0530

    ayaan

---
+++ src/models/flight.js
@@ -0,0 +1,49 @@
+'use strict';
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flight extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Flight.init({
+    flightNumber: {
+      type : DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    airplaneId: {
+      type : DataTypes.INTEGER,
+      allowNull: false
+    },
+    departureAirportId: {
+      type : DataTypes.INTEGER,
+      allowNull: false
+    },
+    arrivalAirportId: {
+      type : DataTypes.INTEGER,
+      allowNull: false
+    },
+    arrivalTime: {
+      type : DataTypes.DATE,
+      allowNull: false
+    },
+    departureTime: {
+      type : DataTypes.DATE,
+      allowNull: false
+    },
+    price: {
+      type : DataTypes.FLOAT,
+      allowNull: false
+    },
+    boardingGate: DataTypes.STRING,
+    totalSeats: {
+      type : DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Flight',
+  });
+  return Flight;
+};
